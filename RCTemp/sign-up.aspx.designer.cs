2287b779ea87ae829bf52d92581ccccc7e7e898a
Omega Fullard <139386007+OmegaFullard@users.noreply.github.com>
2025-07-15 12:30:52 -0400

    update signup and sign in pages
    removed toggle dark/light. padding style

--- RCTemp/sign-up.aspx.designer.cs
+++ RCTemp/sign-up.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace RCTemp
 
     public partial class sign_up
     {
+
+        /// <summary>
+        /// signinHyperLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink signinHyperLink;
     }
 }
